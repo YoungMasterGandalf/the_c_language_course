4e9e88cfb2f324cb12257ea38fdbe5024c68f1f3
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-02-26 09:19:30 +0100

    Ells to cm task

---
+++ mid_lecture_scripts/lecture_2/ell_in_cm.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ell\tInches\tCentimeters\n");
+    
+    int i = 1;
+    int inch;
+    float cm;
+
+    while (i < 31) {
+        if (i%11 == 0) {
+            int j = 0;
+            while (j<20) {
+                printf("-");
+            j++;
+            }
+            printf("\n");
+        }
+        inch = i * 45;
+        cm = inch * 2.54;
+        printf("%d\t%d\t%5.2f\n", i, inch, cm);
+        i++;
+    }
+}
