a4c9cabd2817fe0cc8e8895f6d2a77087e9d6d98
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-03-09 23:01:57 +0100

    Add some lecture 2 scripts

---
+++ mid_lecture_scripts/lecture_2/count_chars.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
+    n0 = n1 = n2 = n3 = n4 = n5 = n6 = n7 = n8 = n9;
+    char c;
+
+    while (1) {
+        if (c == EOF)
+            break;
+
+        if (c == '0')
+            ++n0
+
+        if (c == '1')
+            ++n1
+    }
+}
