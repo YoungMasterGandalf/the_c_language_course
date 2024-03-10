deda7c4832a3a4d8d4cdabbca0a2148777d0c45f
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-03-10 21:01:26 +0100

    Add additional function prototypes to functions.h

--- homework_assignments/lecture_3/functions.h
+++ homework_assignments/lecture_3/functions.h
@@ -1,9 +1,19 @@
 
+#ifndef ASCII_LEN
+#define ASCII_LEN 128
+
+#endif
+
 #ifndef FUNCTIONS_H
+
 #define FUNCTIONS_H
 
 int is_number(int c);
 
 int is_whitespace_char(int c);
+
+void print_n_stars_in_row(int n);
+
+void print_character_occurences(int occurences[ASCII_LEN]);
 
 #endif
