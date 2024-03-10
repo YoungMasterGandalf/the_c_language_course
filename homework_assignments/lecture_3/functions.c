b87e4131ce5121b0896306006446a8faa36acc29
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-03-10 21:01:50 +0100

    Add function definitions to functions.c

--- homework_assignments/lecture_3/functions.c
+++ homework_assignments/lecture_3/functions.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#include "functions.h"
 
 int is_number (int c) {
     return (c >= '0' && c <= '9');
@@ -5,4 +9,23 @@ int is_number (int c) {
 
 int is_whitespace_char (int c) {
     return (c == ' ' || c == '\t' || c == '\n');
+}
+
+void print_n_stars_in_row (int n) {
+    for (int i = 0; i < n; i++) {
+        printf("*");
+    }
+    printf("\n");
+}
+
+void print_character_occurences (int occurences[ASCII_LEN]) {
+    for (int i = 0; i < ASCII_LEN; i++) {
+        if (isprint(i)) {
+            printf("%c\t", i);
+        } else {
+            printf("NP\t");
+        }
+        int char_count = occurences[i];
+        print_n_stars_in_row(char_count);
+    }
 }
